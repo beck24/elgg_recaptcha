1960055f6c5dca81555fd15601e84d080fe082ea
blackleg <hectorespertpardo@gmail.com>
2017-06-04 20:34:55 +0200

    Add Spanish trasnlation

---
+++ languages/es.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+	'elgg_recaptcha:message:fail' => "Validación de reCaptcha no válida",
+	
+	// settings
+	'elgg_recaptcha:setting:public_key' => "Clave del sitio",
+	'elgg_recaptcha:setting:public_key:help' => "Registrate aquí para obtener las claves: %s",
+	'elgg_recaptcha:setting:private_key' => "Clave secreta",
+	'elgg_recaptcha:setting:private_key:help' => "Registrate aquí para obtener las claves: %s",
+	'elgg_recaptcha:setting:theme' => "Tema",
+	'elgg_recaptcha:theme:option:light' => "Claro",
+	'elgg_recaptcha:theme:option:dark' => "Oscuro",
+	'elgg_recaptcha:setting:size' => "Tamaño",
+	'elgg_recaptcha:size:option:normal' => "Normal",
+	'elgg_recaptcha:size:option:compact' => "Compacto",
+	'elgg_recaptcha:setting:recaptcha_type' => "Tipo",
+	'elgg_recaptcha:recaptcha_type:option:image' => "Imagen",
+	'elgg_recaptcha:recaptcha_type:option:audio' => "Audio",
+	'elgg_recaptcha:setting:recaptcha_actions' => "Acciones Protegidas",
+	'elgg_recaptcha:setting:recaptcha_actions:help' => "Marque la casilla para proteger una acción con recaptcha. Tenga en cuenta que esto sólo funciona para los formularios/acciones que siguen el patrón de denominación de Elgg  (ej. registro).  Algunas acciones no tienen formularios, protegiendo una acción que no tiene formulario asociado hará que la acción sea inutilizable .",
+	
+	'elgg_recaptcha:settings:title:nojs' => "Deshabilitar JavaScript",
+	'elgg_recaptcha:settings:nojs:help' => "Mientras que recaptcha funciona la mayoría de las veces en la mayoría de los navegadores, parece tener problemas en algunos (Firefox móvil por ejemplo). Si experimenta problemas en una plataforma/navegador específico, marque la casilla que le corresponde para usar una versión más compatible (aunque ligeramente menos amigable) de la recaptcha. Su plataforma actual es <strong>%s</strong> y su navegador actual es <strong>%s</strong>",
+);
